5f0dfff528b0139d2e1ab10ee16a369a8a6617a3
newtonjose <josephnewton001@gmail.com>
2020-04-22 14:19:29 -0300

    solve partial request entity too large

--- file-upload/functions/nodejs-express/src/server.js
+++ file-upload/functions/nodejs-express/src/server.js
@@ -7,7 +7,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' }));
 
 app.use(express.static('public'));
 
@@ -19,12 +20,12 @@ app.post('/hello', async (req, res) => {
 });
 
 const fileUpload = async (req, res, next) => {
-    const { name, type, base64str } = req.body.input;
-    let fileBuffer = Buffer.from(base64str, 'base64');
-    try {
-      fs.writeFileSync("./public/files/" + name, fileBuffer, 'base64');
-      // insert into db
-      const HASURA_MUTATION = `
+  const { name, type, base64str } = req.body.input;
+  let fileBuffer = Buffer.from(base64str, 'base64');
+  try {
+    fs.writeFileSync("./public/files/" + name, fileBuffer, 'base64');
+    // insert into db
+    const HASURA_MUTATION = `
       mutation ($file_path: String!) {
       	insert_files_one(object: {
       		file_path: $file_path
@@ -32,37 +33,37 @@ const fileUpload = async (req, res, next) => {
       		id
       	}
       }
-      `;
-      const variables = { file_path: "/files/" + name };
+    `;
+    const variables = { file_path: "/files/" + name };
 
-  	  // execute the parent mutation in Hasura
-  	  const fetchResponse = await fetch(
-  	    "http://localhost:8080/v1/graphql",
-  	    {
-  	      method: 'POST',
-  	      body: JSON.stringify({
-  	        query: HASURA_MUTATION,
-  	        variables
-  	      })
-  	    }
-  	  );
-    	  const { data, errors } = await fetchResponse.json();
-    	  console.log(data);
+    // execute the parent mutation in Hasura
+    const fetchResponse = await fetch(
+      "http://localhost:8080/v1/graphql",
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          query: HASURA_MUTATION,
+          variables
+        })
+      }
+    );
+    const { data, errors } = await fetchResponse.json();
+    console.log(data);
 
-    	  // if Hasura operation errors, then throw error
-  	  if (errors) {
-  	    return res.status(400).json({
-  	      message: errors.message
-  	    })
-  	  }
+    // if Hasura operation errors, then throw error
+    if (errors) {
+      return res.status(400).json({
+        message: errors.message
+      })
+    }
 
-  	  // success
-      return res.json({ file_path: "/files/"+name} );
+    // success
+    return res.json({ file_path: "/files/" + name });
   } catch (e) {
-      next(e);
+    next(e);
   }
 }
 
-app.post('/fileUpload', fileUpload) 
+app.post('/fileUpload', fileUpload)
 
 app.listen(PORT);
